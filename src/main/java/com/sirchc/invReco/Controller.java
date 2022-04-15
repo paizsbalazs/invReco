f210b4454fd5af87f3097c681727e66ae6da4dac
paizsbalazs <GdecodeRPaizs1983>
2022-04-15 13:24:43 +0200

    version 0.0.0.7
    - 3099 - es port, és az új adatbázis útvonalra átálltam

--- src/main/java/com/sirchc/invReco/Controller.java
+++ src/main/java/com/sirchc/invReco/Controller.java
@@ -62,7 +62,7 @@ public class Controller {
         Class.forName("org.firebirdsql.jdbc.FBDriver");
 
         Connection connection = DriverManager.getConnection(
-                "jdbc:firebirdsql://localhost:3050/c:/invRecBackend/INVRECO.fdb",
+                "jdbc:firebirdsql://localhost:3099/C:/invReco/release/INVRECO.fdb",
                 "SYSDBA", "masterkey");
 
         Dao dao = new Dao(connection);
@@ -77,7 +77,7 @@ public class Controller {
         Class.forName("org.firebirdsql.jdbc.FBDriver");
 
         Connection connection = DriverManager.getConnection(
-                "jdbc:firebirdsql://localhost:3050/c:/invRecBackend/INVRECO.fdb",
+                "jdbc:firebirdsql://localhost:3099/C:/invReco/release/INVRECO.fdb",
                 "SYSDBA", "masterkey");
 
         Dao dao = new Dao(connection);
@@ -90,7 +90,7 @@ public class Controller {
         Class.forName("org.firebirdsql.jdbc.FBDriver");
 
         Connection connection = DriverManager.getConnection(
-                "jdbc:firebirdsql://localhost:3050/c:/invRecBackend/INVRECO.fdb",
+                "jdbc:firebirdsql://localhost:3099/C:/invReco/release/INVRECO.fdb",
                 "SYSDBA", "masterkey");
 
         Dao dao = new Dao(connection);
