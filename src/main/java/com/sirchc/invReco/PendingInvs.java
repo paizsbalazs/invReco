5a906505e3e9578deb11109663fdc171deb33c9f
paizsbalazs <GdecodeRPaizs1983>
2022-06-05 09:32:02 +0200

    version 0.0.1.5
    - getPendingInvs lekérés bővítése a GETIN értékkel
    - napló bekapcsolása

--- src/main/java/com/sirchc/invReco/PendingInvs.java
+++ src/main/java/com/sirchc/invReco/PendingInvs.java
@@ -3,18 +3,18 @@ package com.sirchc.invReco;
 public class PendingInvs {
 
     public String id;
+    public String getin;
     public String status;
     public String descr;
     public String invtype;
     public String supvatnum;
-    public String getin;
 
-    public PendingInvs(String id, String status, String descr, String invtype, String supvatnum, String getin) {
+    public PendingInvs(String id, String getin, String status, String descr, String invtype, String supvatnum) {
         this.id = id;
+        this.getin = getin;
         this.status = status;
         this.descr = descr;
         this.invtype = invtype;
         this.supvatnum = supvatnum;
-        this.getin = getin;
     }
 }
